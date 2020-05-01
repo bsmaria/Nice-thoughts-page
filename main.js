3ecdbd67dec0c5c9dde2b049e479d78cd84edfb2
Maria Briceño <61625430+bsmaria@users.noreply.github.com>
2020-05-01 01:12:43 -0300

    update main.js

--- main.js
+++ main.js
@@ -6,5 +6,6 @@ const phrases = ["hrlloe", "2","3", "4","5", "6", "7", "8", "9", "10"];
 var random = Math.floor(Math.random() * 10);
 
 btn.addEventListener("click", function() {
+  console.log("nice clicking");
     board.innerHTML = phrases[random];
 });
