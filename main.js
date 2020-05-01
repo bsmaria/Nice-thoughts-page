4c65983a1ba83bc190996b47002ce48a8547132d
Maria Briceño <61625430+bsmaria@users.noreply.github.com>
2020-05-01 01:21:39 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const btn = document.getElementById("random");
 const board = document.getElementById("nice-thoughts");
+let btext = document.getElementById("text");
 
 const phrases = ["hrlloe", "2","3", "4","5", "6", "7", "8", "9", "10"];
 
-var random = Math.floor(Math.random() * 10);
+
 
 btn.addEventListener("click", function() {
-  console.log("nice clicking");
-    board.innerHTML = phrases[random];
+  let random = Math.floor(Math.random() * 10);
+    btext = phrases[random];
 });
