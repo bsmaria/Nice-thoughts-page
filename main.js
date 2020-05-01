9780d3512f2c4748b1b7d751043d9b8b3dd1adc2
Maria Briceño <61625430+bsmaria@users.noreply.github.com>
2020-05-01 01:05:14 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const btn = document.getElementById("random-quote");
 const board = document.getElementById("nice-thoughts");
 
-const phrases = ["", "","","","", ""];
+const phrases = ["1", "2","3", "4","5", "6", "7", "8", "9", "10"];
 
 const random = Math.floor(Math.random() * 10);
 
 btn.addEventListener("click", function() {
-    
+    board.innerHTML = phrases[random];
 });
