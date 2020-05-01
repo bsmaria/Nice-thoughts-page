f572933a6c19acb6e866ef48f5876e3608466321
Maria Briceño <61625430+bsmaria@users.noreply.github.com>
2020-04-30 23:50:36 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("random-quote");
 const board = document.getElementById("nice-thoughts");
 
+const phrases = ["", "","","","", ""];
+
 const random = Math.floor(Math.random() * 10);
 
 btn.addEventListener("click", function() {
