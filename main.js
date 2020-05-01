57a56932f72246da72c441160e95c55e2f5bd96f
bsMaria <bmaria.contact@gmail.com>
2020-04-30 23:12:05 -0300

    stylesheets completed/Js starting

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("random-quote");
+const board = document.getElementById("nice-thoughts");
+
+const random = Math.floor(Math.random() * 10);
+
+btn.addEventListener("click", function() {
+    
+});
