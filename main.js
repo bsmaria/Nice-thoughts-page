fb295e30d88a8882517e1c31e7ad055ae3c02453
bsMaria <bmaria.contact@gmail.com>
2020-05-03 13:06:53 -0300

    javascript works

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const btn = document.getElementById("random");
-const board = document.getElementById("nice-thoughts");
-let btext = document.getElementById("text");
+var btn = document.getElementById("random");
+var board = document.getElementById("nice-thoughts");
+var btext = document.getElementById("text");
 
-const phrases = ["hrlloe", "2","3", "4","5", "6", "7", "8", "9", "10"];
-
+const phrases = ["So friends can really be a strenght in our lives, you know? Keep them close, you don't wanna be weak without them.",
+"Productivity is over rated, peace of mind underapreciated.",
+"There really is no preasure to do anything, Try not to rush, take your time.",
+"That bad thing your constantly thinking? Stop it, man, have an ice cream, damn it.",
+"Dude... Is ok.",
+"There's time for everything you want to do.",
+"Take 5 minutes to just breath, then go at it again.",
+"Have a cup of tea, incredibly, it does help.",
+"Life can be awfull, but we're together in it.",
+"Let's be sad together, that's nice."];
 
 
 btn.addEventListener("click", function() {
   let random = Math.floor(Math.random() * 10);
-    board.innerHTML = phrases[random];
+    btext.innerHTML = phrases[random];
 });
